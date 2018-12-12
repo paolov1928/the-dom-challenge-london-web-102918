84269038032784f7a2ef3a2c246dd1bd861c6ead
github username <github email address>
2018-12-12 18:15:41 +0000

    Added counter and decrement

--- challenge.js
+++ challenge.js
@@ -1,4 +1,15 @@
 const counter = document.querySelector("#counter")
+const plusbutton = document.querySelector("#add")
+const subtractbutton = document.querySelector("#subtract")
+
+
+function addToCounter () {
+  plusbutton.addEventListener("click", incrementCounter)
+}
+
+function subtractToCounter () {
+  subtractbutton.addEventListener("click", decrementCounter)
+}
 
 function loadCounter () {
   counter.addEventListener("load", setInterval)
@@ -8,4 +19,31 @@ function incrementCounter() {
   counter.innerText++
 }
 
+function decrementCounter() {
+  counter.innerText--
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 setInterval(incrementCounter, 1000);
+addToCounter();
+subtractToCounter();
